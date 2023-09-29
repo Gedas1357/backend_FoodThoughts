035250a284603ef0b939d79b846cf4e4059d21ea
Gedas1357 <gedasvilnietis@gmail.com>
2023-09-29 11:08:14 +0300

    Added http get Ingredients

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("ingredient/<int:id>", views.ingredient, name="index"),
+    path("ingredients/", views.ingredients, name="index"),
     path("recipe/<int:id>", views.recipe, name="index"),
     path("recipe_ingredient/<int:id>", views.recipe_ingredient, name="index"),
 ]
--- backend/main/views.py
+++ backend/main/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import Ingredient, Recipe, Recipe_Ingredient
+import json
+
 # Create your views here.
 def ingredient(response, id):
     ls = Ingredient.objects.get(id=id)
     return HttpResponse("%s" %ls.name)
+
+def ingredients(response):
+    ls = Ingredient.objects.all()
+    data = [{'id': item.id, 'name': item.name} for item in ls]
+    return JsonResponse(data, safe=False)
+
 def recipe(response, id):
     ls = Recipe.objects.get(id=id)
     return HttpResponse("%s" %ls.name)
