8d1488ce8fa8b4288b7f375b2016664bd3546a0b
Gedas1357 <gedasvilnietis@gmail.com>
2023-10-02 15:49:30 +0300

    Added Ingredient DELETE endpoint

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("ingredient/<int:id>", views.getIngredient, name="getIngredient"),
     path("ingredients/", views.getIngredients, name="getIngredients"),
     path("ingredient/post/", views.postIngredient, name="postIngredient"),
+    path("ingredient/delete/", views.deleteIngredient, name="deleteIngredient"),
     path("recipe/<int:id>", views.recipe, name="getRecipe"),
     path("recipe_ingredient/<int:id>", views.recipe_ingredient, name="getRecipe_ingredient"),
 ]
--- backend/main/views.py
+++ backend/main/views.py
@@ -10,30 +10,40 @@ from rest_framework.decorators import api_view
 # Create your views here.
 @api_view(['GET'])
 def getIngredient(response, id):
-    ls = Ingredient.objects.get(id=id)
+    ls = Ingredient.objects.get(id = id)
     return HttpResponse("%s" %ls.name)
 
 @api_view(['GET'])
 def getIngredients(response):
     ingredients = Ingredient.objects.all()
-    serializer = IngredientSerializer(ingredients, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    serializer = IngredientSerializer(ingredients, many = True)
+    return JsonResponse(serializer.data, safe = False)
 
 @api_view(['POST'])
 def postIngredient(request):
+    print(request.data)
     serializer = IngredientSerializer(data = request.data)
     if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data, status = 201)
+    return Response(serializer.errors, status = 400)
+
+@api_view(['DELETE'])
+def deleteIngredient(request):
+    try:
+        instance = Ingredient.objects.get(id = request.data.get("id"))
+        instance.delete()
+        return Response(status = 200)
+    except Ingredient.DoesNotExist:
+        return Response(status = 404)
 
 
 @api_view(['GET'])
 def recipe(response, id):
-    ls = Recipe.objects.get(id=id)
+    ls = Recipe.objects.get(id = id)
     return HttpResponse("%s" %ls.name)
 
 @api_view(['GET'])
 def recipe_ingredient(response, id):
-    ls = Recipe_Ingredient.objects.get(id=id)
+    ls = Recipe_Ingredient.objects.get(id = id)
     return HttpResponse("%s" %ls.name)
